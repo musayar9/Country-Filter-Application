6c68cc6cc8e3778fd1066ffcc2b9751db1d94ce5
Musa Sayar <musasayar67@gmail.com>
2024-02-07 15:41:17 +0300

    add information

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,10 +6,15 @@ const Form = () => {
       <div className=" flex p-3 max-w-5xl mx-auto flex-col md:flex-row md:items-center md:justify-center gap-4">
         {/*Left Side */}
         <div className="flex-1 p-5">
-          <img className="drop-shadow-lg " src={worldImage} />
-          <p className="text-sm mt-5 text-white">
-            This is a demo project. You can sign ın with your email and password
-            or with google
+          <img className="drop-shadow-lg  " src={worldImage} />
+          <p className="text-xs mt-5 ml-4 text-white flex items-start justify-center flex-col space-y-2 tracking-tight">
+            <span>Filtering by entered dollar exchange rate</span>
+            <span>
+              Grouping data filtered by dollar exchange rate according to the
+              entered number value
+            </span>
+
+            <span>Filtering by country group value</span>
           </p>
         </div>
 
